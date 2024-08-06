b5630bbb668a6862c47be0851ea856c527d83575
Andrej Aleksic <you@example.com>
2024-08-06 13:45:44 +0200

    Responsivness fix for new layout

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ $(document).ready(function() {
         { src: 'img/image10.jpeg', description: 'Description for image 10' },
         { src: 'img/image11.jpeg', description: 'Description for image 11' },
         { src: 'img/image12.jpeg', description: 'Description for image 12' },
+        { src: 'img/image1.jpeg', description: 'Description for image 1' },
+        { src: 'img/image2.jpeg', description: 'Description for image 2' },
+        { src: 'img/image3.jpeg', description: 'Description for image 3' },
+        { src: 'img/image4.jpeg', description: 'Description for image 4' },
+        { src: 'img/image5.jpeg', description: 'Description for image 5' },
+        { src: 'img/image6.jpeg', description: 'Description for image 6' },
+        { src: 'img/image7.jpeg', description: 'Description for image 7' },
+        { src: 'img/image8.jpeg', description: 'Description for image 8' },
+        { src: 'img/image9.jpeg', description: 'Description for image 9' },
+        { src: 'img/image10.jpeg', description: 'Description for image 10' },
+        { src: 'img/image11.jpeg', description: 'Description for image 11' },
+        { src: 'img/image12.jpeg', description: 'Description for image 12' }
     ];
 
 
